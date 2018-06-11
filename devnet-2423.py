f4ce45698aa9b6dbf20e34a43070a49b4c2c7990
Tim Garner <timgarner0@gmail.com>
2018-06-11 15:45:06 -0400

    infra code

--- devnet-2423.py
+++ devnet-2423.py
@@ -17,3 +17,133 @@ Remi Philippe <rephilip@cisco.com>
 
 https://github.com/tetration-exchange/devnet-2423-2018/
 """
+
+import json
+import os, sys
+import time
+from tetpyclient import RestClient
+from requests.packages.urllib3 import disable_warnings
+
+API_ENDPOINT = "https://vcluster01.insbu.net"
+CRED_FILE = "./api_credentials.json"
+APP_ID = "5b1eb5d1755f0263cd4218f3"
+FILTER_ID = "5b1eb5ff497d4f1a942454d0"
+
+
+# Might be useful to connect somewhere, we'll put all the connect logic here
+def connect():
+    # Check credentials file exists
+    if os.path.exists(CRED_FILE) == False:
+        sys.exit("Error! Credentials file is not present")
+    disable_warnings()
+
+    # Class Constructor
+    rc = RestClient(API_ENDPOINT, credentials_file=CRED_FILE, verify=False)
+    return rc
+
+
+# I hate implementing try catch everywhere, will create a wrapper function
+# this function only implements some basic verifications
+def query(method, endpoint, payload=""):
+    try:
+        if method == "POST":
+            resp = rc.post(endpoint, json_body=json.dumps(payload))
+        elif method == "GET":
+            resp = rc.get(endpoint)
+
+        if resp.status_code != 200:
+            raise Exception("Status Code - " + str(resp.status_code) +
+                            "\nError - " + resp.text + "\n")
+    except Exception as e:
+        print e
+        #sys.exit(1)
+
+    if resp.status_code == 200:
+        try:
+            return resp.json()
+        except ValueError:
+            return True
+
+
+def get_sensors():
+    sensors = {}
+    # DEVNET Code Start, tag=sensors
+    all_sensors = query("GET", "/sensors")
+    for s in all_sensors['results']:
+        # I don't care about the Tetration hosts for now, let's ignore them
+        if not any(d.get('vrf', None) == 'Tetration' for d in s['interfaces']):
+            interfaces = filter(
+                lambda i: i['family_type'] == 'IPV4' and i['ip'] != '127.0.0.1',
+                s['interfaces'])
+            sensors[s['uuid']] = {
+                "hostname": s['host_name'],
+                "uuid": s['uuid'],
+                "interfaces": list(map(lambda x: {"ip": x['ip'], "type": x['family_type'], "vrf": x['vrf'], "mac": x['mac']}, interfaces)),
+                "inactive": int(time.time()) - s['last_config_fetch_at'] > 1805
+            }
+    # DEVNET Code End
+    return sensors
+
+
+def get_filter_members(filter_id):
+    filter_query = query("GET", "/filters/inventories/" + filter_id)
+    filter_members = query(
+        "POST", "/inventory/search", payload={'filter': filter_query['query']})
+    results = {
+        "name":
+        filter_query['name'],
+        "results":
+        filter(lambda f: f["address_type"] == "IPV4",
+               filter_members["results"])
+    }
+    return results
+
+
+def get_application(app_id):
+    retval = {}
+    # DEVNET Code Start, tag=application
+    # let's get the app details...
+    details = query("GET", "/applications/" + app_id + "/details")
+
+    # ...and the scope details...
+    scope = query("GET", "/app_scopes/" + details['app_scope_id'])
+
+    # what are my external dependencies?
+    d = details.get('inventory_filters')
+    if d == None:
+        d = []
+    # this can also be written as details.get('inventory_filters', [])
+
+    # who is part of my application?
+    c = details.get('clusters')
+    if c == None:
+        c = []
+    # this can also be written as details.get('clusters', [])
+
+    # make it pretty!
+    retval = {
+        "scope": {k: v for k, v in scope.iteritems() if k == "id" or k == "name" or k == "parent_app_scope_id"},
+        "name": details['name'],
+        "id": app_id,
+        "external": list(map(lambda x: {"id": x['id'], "name": x['name']}, d)),
+        "clusters": list(map(lambda x: {"id": x['id'], "name": x['name'], "external": x['external'], "nodes": x['nodes']}, c)),
+        "policies": details['absolute_policies']
+    }
+    # DEVNET Code End
+    return retval
+
+
+def mark_as_inactive(ip):
+    query(
+        "POST",
+        "/inventory/tags/Default",
+        payload={
+            "ip": ip,
+            "attributes": {
+                "inactive": "true"
+            }
+        })
+
+
+def get_inactive_sensors(sensors):
+    return filter(lambda s: s['inactive'], sensors.values())
